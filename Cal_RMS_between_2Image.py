90113cafc4d6ef69a93b6b22ecd8dc4fa01f15da
Vincent Chen <403930045@qq.com>
2018-06-27 16:24:17 +0800

    Update Cal_RMS_between_2Image.py

--- Cal_RMS_between_2Image.py
+++ Cal_RMS_between_2Image.py
@@ -6,13 +6,15 @@ Created on Fri Jun 22 15:33:44 2018
 
 https://en.wikipedia.org/wiki/Root_mean_square
 
+1. Update in 20180627: normalize RMS 
+
 @author: vincchen
 """
 
 from __future__ import division
 import numpy as np
 
-def cal_rms(image_array_1, image_array_2):
+def cal_rms(image_array_1, image_array_2，fg ,bg):
     shape_1 = image_array_1.shape
     shape_2 = image_array_2.shape
     if shape_1 != shape_2:
@@ -22,5 +24,6 @@ def cal_rms(image_array_1, image_array_2):
         Diff = np.subtract(image_array_1, image_array_2)
         Size = Diff.size # Size = H * W
         Sum_of_Squares = np.vdot(Diff, Diff)
-        RMS = np.sqrt(Sum_of_Squares / Size)
+        rms = np.sqrt(Sum_of_Squares / Size) # dimension of rms is the intensity of pixel 
+        RMS = rms / (abs(fg-bg))             # dimension of RMS is the %, RMS is normalized 
         return RMS
